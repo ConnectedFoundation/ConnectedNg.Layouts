1b69fe82c7e9bbd10a169d706b4d3b9fb4f8e1e7
Jordan-Tompit <janko.jordan@tompit.com>
2025-06-24 16:13:53 +0200

    feat: delete stale files

--- src/projects/connected-ng/layouts/src/main-layout/drawer-content/drawer-content.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'cf-main-layout-drawer-content',
-  template: '<ng-content></ng-content>'
-})
-export class DrawerContentComponent {
-
-}
--- src/projects/connected-ng/layouts/src/main-layout/side-bar-content/side-bar-content.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'cf-main-layout-side-bar-content',
-  template: '<ng-content></ng-content>'
-})
-export class SideBarContentComponent {
-
-}
